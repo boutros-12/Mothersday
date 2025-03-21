022616f93e9b3229ed24d2757cc6ac8536f62d0b
boutros-12 <boutroslaham12345@gmail.com>
2025-03-21 10:42:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-// Get the popup and link elements
-const popup = document.getElementById("popup");
-const popupLink = document.getElementById("popupLink");
-const closeBtn = document.getElementById("closeBtn");
-
-// Show the popup when the link is clicked
-popupLink.onclick = function() {
-    popup.style.display = "block";
-}
-
-// Close the popup when the close button is clicked
-closeBtn.onclick = function() {
-    popup.style.display = "none";
-}
-
-// Close the popup if the user clicks anywhere outside the popup content
-window.onclick = function(event) {
-    if (event.target == popup) {
-        popup.style.display = "none";
-    }
+function playMusic() {
+    let music = document.getElementById("bg-music");
+    music.play();
+    alert("Enjoy the music! 🎶💖");
 }
